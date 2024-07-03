ee37dfa99e848a5c2edfc55b0b5c3742a665bfff
MartinKahabka <116288221+MartinKahabka@users.noreply.github.com>
2024-07-03 17:47:04 +0200

    finished file
    added analysis of vcfs and saving in file
    not tested yet

--- promoter/variantPromoterRegion.py
+++ promoter/variantPromoterRegion.py
@@ -1,5 +1,6 @@
 import argparse
 from functools import cmp_to_key
+import os
 
 # run with comment
 #python3
@@ -12,7 +13,7 @@ def chrToNumber(chrom):
     else:
         return int(chrom[3:])
 
-def sortPromoter(x1, x2):
+def sortGenePos(x1, x2):
     # get chr
     chr1 = chrToNumber(x1[0])
     chr2 = chrToNumber(x2[0])
@@ -31,6 +32,23 @@ def sortPromoter(x1, x2):
             return 1
     # pos are exactly the same
     return 0
+
+def variantInBound(start, end, prom, variant) -> str:
+    # -1 if promoter is downstream of variant, 1 if otherwise, 0 else
+    relPos = sortGenePos(prom, variant)
+    promPos = prom[1]
+    variantPos = variant[1]
+    # check if chromosoms are equal
+    if chrToNumber(prom[0]) == chrToNumber(variant[0]): 
+        # define bound and check
+        downBound = promPos- start
+        upBound = promPos +  end
+        if  downBound <= variantPos <= upBound:
+            return "in"
+        else:
+            return "bigger" if relPos == 1 else "smaller"
+    else:
+        return "bigger" if relPos == 1 else "smaller"
             
 
 parser = argparse.ArgumentParser(prog='variantPromoterRegion.py', description='Description of your script')
@@ -48,13 +66,14 @@ name = args.name_process
 output_path = args.output_dir
 vcf_path = args.vcf_path
 promoter_path = args.path_promoters
-start_prom = args.start
-end_prom = args.end
+start_prom = int(args.start)
+end_prom = int(args.end)
 
 
 # safe promoter regions as tuple (chromosome, position TSS)
 promoter_regions = []
 
+# read in promoter regions
 with open(promoter_path, 'r') as file:
     # get promoters regions of all promoters
     for line in file:
@@ -65,6 +84,36 @@ with open(promoter_path, 'r') as file:
             # add to promoter regions
             promoter_regions.append((chrom, pos))
             
-# if regions arent sorted
-sorter = cmp_to_key(sortPromoter)
+# sort in case promoter regions aren't sorted
+sorter = cmp_to_key(sortGenePos)
 promoter_regions.sort(key = sorter)
+
+# name output file
+filename_vcf = os.path.basename(vcf_path)
+full_output_path = os.path.join(output_path, name + "_promoterVcfs_" + filename_vcf)
+print(full_output_path)
+
+# read in vcf of patient
+with open(vcf_path, 'r') as vcf_file, open(full_output_path, 'w') as filter_vcfs_file:
+    # write information into promoter vcf file
+    filter_vcfs_file.write("# Name of file: " + os.path.basename(full_output_path))
+    filter_vcfs_file.write("# Name of process: " + name)
+    filter_vcfs_file.write("# Patient vcf file path: " + vcf_path)
+    filter_vcfs_file.write("# Promoter region file path:" + promoter_path)
+    filter_vcfs_file.write("# Length Down/Upstream region of promoter TSS side: " + str(start_prom) + "/" + str(end_prom))
+    # keep pointer on promoter, due to sorted arrays in O(n), n : num Vcfs in vcf file
+    pointer_promoter = 0
+    for line in vcf_file:
+        if line[0] != '#':
+            contain = line.split('\t')
+            # (chromosome, position, ref, alt)
+            variant = (contain[0], contain[1], contain[3], contain[4])
+            # returns smaller, in or bigger. Relative pos of promoter to variant
+            relPos = variantInBound(start_prom, end_prom, promoter_regions[pointer_promoter], variant)
+            # step to next promoter if current is downstream
+            if relPos == "smaller":
+                while variantInBound(start_prom, end_prom, promoter_regions[pointer_promoter], variant) == "smaller":
+                    pointer_promoter += 1
+            
+            if relPos == "in":
+                filter_vcfs_file.write(line)
