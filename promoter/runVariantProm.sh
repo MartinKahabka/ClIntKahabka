2e925ff7f25e73b82e898eb2df5422a7a7976283
MartinKahabka <116288221+MartinKahabka@users.noreply.github.com>
2024-07-05 17:12:47 +0200

    updated run var

--- promoter/runVariantProm.sh
+++ promoter/runVariantProm.sh
@@ -10,9 +10,13 @@ echo "end: $6"
 # pattern matching for dna_vcf files
 pattern = "*.vcf"
 files=$(find "input_path" -name "$pattern")
+full_output_path = "$input_path/$name"
 
 for file in $files; do
     full_path="$input_folder/$file"
 
     # run script
-    python3 "variantPromoterRegion.py" "-n $name" "-o $output_dir" "-v $full_path" "-p $promoter_path" "-s $start" "-e "$end"
+    python3 "variantPromoterRegion.py" "-n $name" "-o $full_output_path" "-v $full_path" "-p $promoter_path" "-s $start" "-e "$end"
+
+# python3 variantPromoterRegion.py -n "alpha" -o "informationAndData/vcfsPromoter/" -v "informationAndData/test_vcf_file" -p informationAndData/GRCh37_promoterChrPos_testCopy.bed -s "500" -o "100"
+#
