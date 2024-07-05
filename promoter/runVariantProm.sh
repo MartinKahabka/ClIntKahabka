87e6bd42df2b133bd86879085985b6b40690de1b
MartinKahabka <116288221+MartinKahabka@users.noreply.github.com>
2024-07-05 19:10:10 +0200

    updated sh file

--- promoter/runVariantProm.sh
+++ promoter/runVariantProm.sh
@@ -7,16 +7,31 @@ echo "promoter_path: $4"
 echo "start: $5"
 echo "end: $6"
 
+name=$1
+output_path=$2
+input_path=$3
+promoter_path=$4
+start=$5
+end=$6
+
 # pattern matching for dna_vcf files
-pattern = "*.vcf"
-files=$(find "input_path" -name "$pattern")
-full_output_path = "$input_path/$name"
+pattern="*.vcf"
+files=$(find "$input_path" -name "$pattern")
+full_output_path="$output_path/$name"
+
+# create dir, check for output dir
+if [ ! -d "$output_path"]; then
+    mkdir "$output_path"
+fi
+
+# create subfolder
+mkdir -p "$full_output_path"
 
 for file in $files; do
-    full_path="$input_folder/$file"
-
+    full_input_path="$input_path/$file"
+    echo filename "$file"
     # run script
-    python3 "variantPromoterRegion.py" "-n $name" "-o $full_output_path" "-v $full_path" "-p $promoter_path" "-s $start" "-e "$end"
-
+    python3 "variantPromoterRegion.py" -n "$name" -o "$full_output_path" -v "$file" -p "$promoter_path" -s "$start" -e "$end"
+done
 # python3 variantPromoterRegion.py -n "alpha" -o "informationAndData/vcfsPromoter/" -v "informationAndData/test_vcf_file" -p informationAndData/GRCh37_promoterChrPos_testCopy.bed -s "500" -o "100"
 #
