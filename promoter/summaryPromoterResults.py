2841c6eca83494335b3c1ca154bbab4cef8b12b1
MartinKahabka <116288221+MartinKahabka@users.noreply.github.com>
2024-07-06 19:36:16 +0200

    minor changes

--- promoter/summaryPromoterResults.py
+++ promoter/summaryPromoterResults.py
@@ -4,20 +4,11 @@ import re
 
 # author: Martin Kahabka
 
-# this file should input the output if "runVariantProm.sh" and return a tsv as described on git
-# necessary files
-# 1. from output files of "runVariantProm.sh" get
-#   1.1 chr/pos of variants
-#   1.2 number of patients with variant
-#   1.3 ID of patients
-# 3. from Q001H_sample_preparations_20230803115337.tsv get
-#   3.1 condition (severe/not severe) of patients
-
-# structure
-# save ID of each analysed patient
-# get condition of each patient with saved ID
-# counter number of severe/not severe patients
-# counter number of 
+# structure - can be removed later
+# save ID of each analysed patient (c)
+# get condition of each patient with saved ID (c)
+# counter number of severe/not severe patients (c)
+# counter number of  (c)
 # for each output file get
 #   ID of patient -> condition of patient
 #   for each variant do
